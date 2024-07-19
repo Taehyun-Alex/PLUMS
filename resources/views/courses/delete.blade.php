54e8ddee1553d2fae572c83df5ab8e1c4f4f10b0
jaydenstokes <20125497@tafe.wa.edu.au>
2024-07-19 12:03:41 +0800

    More style changes

--- resources/views/courses/delete.blade.php
+++ resources/views/courses/delete.blade.php
@@ -20,8 +20,7 @@
                     class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-purple-500 focus:ring focus:ring-purple-500 focus:ring-opacity-50"
                 >{{ $course["description"] ?? "" }}</p>
             </div>
-            {{-- For some reason, gap-2 isn't working and only gap-4+ works... don't know why --}}
-            <div class="flex flex-row items-center justify-center gap-4">
+            <div class="flex flex-row items-center justify-center gap-2">
                 <button type="submit" class="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded-md transition duration-300">Delete Course</button>
                 <a href="{{ route('dashboard') }}" class="bg-purple-600 hover:bg-purple-700 text-white py-2 px-4 rounded-md transition duration-300">Cancel</a>
             </div>
