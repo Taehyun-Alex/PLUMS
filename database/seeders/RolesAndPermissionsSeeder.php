194ace42628dd3492181a1e98c287cd28810e54a
NorthBridge Trainee <nbt@tdm.local>
2024-08-08 11:16:51 +0800

    Refactor: Roles and permissions are updated and combined in simpler way

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -2,27 +2,52 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        Permission::create(['name' => 'view admins']);
-        Permission::create(['name' => 'edit admins']);
-        Permission::create(['name' => 'create admins']);
-        Permission::create(['name' => 'delete admins']);
+        // Define permissions
+        $permissions = [
+            'view', 'edit', 'create', 'delete', 'browse', 'show', 'add',
+            'trash-recover', 'trash-remove', 'trash-empty', 'trash-restore',
+        ];
 
-        $admin = Role::create(['name' => 'admin']);
-        $admin->givePermissionTo(['view admins', 'edit admins', 'create admins', 'delete admins']);
 
-        $staff = Role::create(['name' => 'staff']);
-        $staff->givePermissionTo(['view admins']);
+        // Create permissions
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        // Define roles and assign existing permissions
+        $adminPermissions = $permission;
+
+        $staffPermissions = [
+            'view'
+        ];
+
+
+        // Create roles and assign permissions
+        $adminRole = Role::create(['name' => 'admin']);
+        $adminRole->givePermissionTo($adminPermissions);
+
+        $staffRole = Role::create(['name' => 'staff']);
+        $staffRole->givePermissionTo($staffPermissions);
+
+
+//        Permission::create(['name' => 'view admins']);
+//        Permission::create(['name' => 'edit admins']);
+//        Permission::create(['name' => 'create admins']);
+//        Permission::create(['name' => 'delete admins']);
+
+
+//        $admin = Role::create(['name' => 'admin']);
+//        $admin->givePermissionTo(['view admins', 'edit admins', 'create admins', 'delete admins']);
+//
+//        $staff = Role::create(['name' => 'staff']);
+//        $staff->givePermissionTo(['view admins']);
     }
 }
