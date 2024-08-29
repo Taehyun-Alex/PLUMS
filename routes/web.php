01a7528403da3045108143bcf1f1066475a1a028
Hyoin Lee <winroof1988@gmail.com>
2024-08-29 23:40:31 +0800

    fix(routes): Fix all routes works

--- routes/web.php
+++ routes/web.php
@@ -34,68 +34,63 @@ Route::middleware('auth')->group(function () {
         return view('settings.settings');
     })->name('settings');
 
-    // Quizzes routes
-    Route::prefix('quizzes')->group(function () {
-        Route::get('/', [QuizController::class, 'index'])->name('quizzes.index');
-        Route::get('/create', [QuizController::class, 'create'])->name('quizzes.create');
-        Route::post('/create', [QuizController::class, 'store'])->name('quizzes.store');
-        Route::get('/{quiz}', [QuizController::class, 'show'])->name('quizzes.show');
-        Route::get('/{quiz}/edit', [QuizController::class, 'edit'])->name('quizzes.edit');
-        Route::put('/{quiz}', [QuizController::class, 'update'])->name('quizzes.update');
-        Route::delete('/{quiz}/delete', [QuizController::class, 'delete'])->name('quizzes.delete');
-        Route::delete('/{quiz}', [QuizController::class, 'destroy'])->name('quizzes.destroy');
 
-        // Quiz trash routes
-        Route::prefix('trash')->group(function () {
-            Route::get('/', [QuizController::class, 'trash'])->name('quizzes.trash');
-            Route::post('/restore/{quiz}', [QuizController::class, 'restore'])->name('quizzes.trash-restore');
-            Route::delete('/remove/{quiz}', [QuizController::class, 'remove'])->name('quizzes.trash-remove');
-            Route::post('/restoreAll', [QuizController::class, 'restoreAll'])->name('quizzes.trash-restore-all');
-            Route::delete('/empty', [QuizController::class, 'empty'])->name('quizzes.trash-empty');
-        });
-    });
+
+
+    // Quiz trash routes
+    Route::get('quizzes/trash', [QuizController::class, 'trash'])->name('quizzes.trash');
+    Route::patch('quizzes/restore/{quiz}', [QuizController::class, 'restore'])->name('quizzes.trash-restore');
+    Route::delete('quizzes/remove/{quiz}', [QuizController::class, 'remove'])->name('quizzes.trash-remove');
+    Route::post('quizzes/restoreAll', [QuizController::class, 'restoreAll'])->name('quizzes.trash-restore-all');
+    Route::delete('quizzes/empty', [QuizController::class, 'empty'])->name('quizzes.trash-empty');
+
+    // Quizzes routes
+    Route::get('quizzes/', [QuizController::class, 'index'])->name('quizzes.index');
+    Route::get('quizzes/create', [QuizController::class, 'create'])->name('quizzes.create');
+    Route::post('quizzes/create', [QuizController::class, 'store'])->name('quizzes.store');
+    Route::get('quizzes/{quiz}', [QuizController::class, 'show'])->name('quizzes.show');
+    Route::get('quizzes/{quiz}/edit', [QuizController::class, 'edit'])->name('quizzes.edit');
+    Route::put('quizzes/{quiz}', [QuizController::class, 'update'])->name('quizzes.update');
+    Route::delete('quizzes/{quiz}/delete', [QuizController::class, 'delete'])->name('quizzes.delete');
+    Route::delete('quizzes/{quiz}', [QuizController::class, 'destroy'])->name('quizzes.destroy');
+
+
+    // Course trash routes
+    Route::get('courses/trash', [CourseController::class, 'trash'])->name('courses.trash');
+    Route::patch('courses/{course}/restore', [CourseController::class, 'restore'])->name('courses.restore');
+    Route::delete('courses/{course}/forceDelete', [CourseController::class, 'forceDelete'])->name('courses.forceDelete');
+    Route::post('courses/restoreAll', [CourseController::class, 'restoreAll'])->name('courses.trash-restore-all');
+    Route::delete('courses/empty', [CourseController::class, 'empty'])->name('courses.trash-empty');
 
     // Courses routes
-    Route::prefix('courses')->group(function () {
-        Route::get('/', [CourseController::class, 'index'])->name('courses.index');
-        Route::get('/create', [CourseController::class, 'create'])->name('courses.create');
-        Route::post('/create', [CourseController::class, 'store'])->name('courses.store');
-        Route::get('/{course}', [CourseController::class, 'show'])->name('courses.show');
-        Route::get('/{course}/edit', [CourseController::class, 'edit'])->name('courses.edit');
-        Route::put('/{course}', [CourseController::class, 'update'])->name('courses.update');
-        Route::get('/{course}/delete', [CourseController::class, 'delete'])->name('courses.delete');
-        Route::delete('/{course}', [CourseController::class, 'destroy'])->name('courses.destroy');
+    Route::get('courses/', [CourseController::class, 'index'])->name('courses.index');
+    Route::get('courses/create', [CourseController::class, 'create'])->name('courses.create');
+    Route::post('courses/create', [CourseController::class, 'store'])->name('courses.store');
+    Route::get('courses/{course}', [CourseController::class, 'show'])->name('courses.show');
+    Route::get('courses/{course}/edit', [CourseController::class, 'edit'])->name('courses.edit');
+    Route::put('courses/{course}', [CourseController::class, 'update'])->name('courses.update');
+    Route::get('courses/{course}/delete', [CourseController::class, 'delete'])->name('courses.delete');
+    Route::delete('courses/{course}', [CourseController::class, 'destroy'])->name('courses.destroy');
 
-        // Course trash routes
-        Route::prefix('trash')->group(function () {
-            Route::get('/', [CourseController::class, 'trash'])->name('courses.trash');
-            Route::patch('/{course}/restore', [CourseController::class, 'restore'])->name('courses.restore');
-            Route::delete('/{course}/forceDelete', [CourseController::class, 'forceDelete'])->name('courses.forceDelete');
-            Route::post('/restoreAll', [CourseController::class, 'restoreAll'])->name('courses.trash-restore-all');
-            Route::delete('/empty', [CourseController::class, 'empty'])->name('courses.trash-empty');
-        });
-    });
+
+    // User trash routes
+    Route::get('/trash', [UserController::class, 'trash'])->name('users.trash');
+    Route::post('/restore/{user}', [UserController::class, 'restore'])->name('users.trash-restore');
+    Route::delete('/remove/{user}', [UserController::class, 'remove'])->name('users.trash-remove');
+    Route::post('/restoreAll', [UserController::class, 'restoreAll'])->name('users.trash-restore-all');
+    Route::delete('/empty', [UserController::class, 'empty'])->name('users.trash-empty');
 
     // Users routes
-    Route::prefix('users')->group(function () {
-        Route::get('/', [UserController::class, 'index'])->name('users.index');
-        Route::get('/create', [UserController::class, 'create'])->name('users.create');
-        Route::post('/create', [UserController::class, 'store'])->name('users.store');
-        Route::get('/{user}', [UserController::class, 'show'])->name('users.show');
-        Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-        Route::put('/{user}', [UserController::class, 'update'])->name('users.update');
-        Route::delete('/{user}/delete', [UserController::class, 'delete'])->name('users.delete');
-        Route::delete('/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::get('users/', [UserController::class, 'index'])->name('users.index');
+    Route::get('users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('users/create', [UserController::class, 'store'])->name('users.store');
+    Route::get('users/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::get('users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('users/{user}/delete', [UserController::class, 'delete'])->name('users.delete');
+    Route::delete('users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
 
-        // User trash routes
-        Route::prefix('trash')->group(function () {
-            Route::get('/', [UserController::class, 'trash'])->name('users.trash');
-            Route::post('/restore/{user}', [UserController::class, 'restore'])->name('users.trash-restore');
-            Route::delete('/remove/{user}', [UserController::class, 'remove'])->name('users.trash-remove');
-            Route::post('/restoreAll', [UserController::class, 'restoreAll'])->name('users.trash-restore-all');
-            Route::delete('/empty', [UserController::class, 'empty'])->name('users.trash-empty');
-        });
-    });
+
 });
 
 // Include authentication routes
