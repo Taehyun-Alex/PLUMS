c09dec688754f5bbefc1da07c79c70b3c664ad56
jayden-tafe <20125497@tafe.wa.edu.au>
2024-09-12 11:23:26 +0800

    Mobile register and logout endpoints

--- app/Http/Controllers/MobileApiController.php
+++ app/Http/Controllers/MobileApiController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Classes\ApiResponseClass;
 use App\Http\Requests\MobileApiLoginRequest;
+use App\Http\Requests\MobileRegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -23,6 +25,23 @@ class MobileApiController extends Controller
         }
 
         return ApiResponseClass::sendResponse([], 'Invalid Credentials', false, 401);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return ApiResponseClass::sendResponse([], 'Logged out successfully');
+    }
+
+    public function register(MobileRegisterRequest $request) {
+        $validated = $request->validated();
+        $user = User::create($validated);
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        return ApiResponseClass::sendResponse([
+            'accessToken' => $token,
+            'type' => 'Bearer'
+        ], 'Account created successfully', true, 201);
     }
 
     public function currentUser(Request $request) {
--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function() {
-    // Mobile Authentication
     Route::post('/mobile/login', [MobileApiController::class, 'login'])->name('mobile.login');
+    Route::post('/mobile/register', [MobileApiController::class, 'register'])->name('mobile.register');
 
-    // Mobile User Routes
     Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/mobile/logout', [MobileApiController::class, 'logout'])->name('mobile.logout');
         Route::get('/mobile/me', [MobileApiController::class, 'currentUser'])->name('mobile.me');
     });
 });
