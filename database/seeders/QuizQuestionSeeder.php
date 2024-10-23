7a1143287e5098529112db84bdb389ae179213c6
cliveeee <20095317@tafe.wa.edu>
2024-10-23 11:01:25 +0800

    fix:seeder issue

--- database/seeders/QuizQuestionSeeder.php
+++ database/seeders/QuizQuestionSeeder.php
@@ -2,10 +2,11 @@
 
 namespace Database\Seeders;
 
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\QuizQuestion;
 
-namespace Database\Seeders;
 
 class QuizQuestionSeeder extends Seeder
 {
