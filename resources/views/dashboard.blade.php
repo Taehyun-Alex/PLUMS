5fae416b24285ee5845acfc56dc90f8ccec732e6
jaydenstokes <20125497@tafe.wa.edu.au>
2024-11-03 04:04:59 +0800

    Small fix to quizCount

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,7 +21,7 @@
                 <div class="flex items-center justify-between">
                     <div>
                         <a href="{{ route('quizzes.index') }}" class="text-lg font-bold text-gray-800">Quizzes</a>
-                        <p class="text-gray-600">Total: 50</p>
+                        <p class="text-gray-600">{{$quizCount}}</p>
                     </div>
                     <svg class="w-8 h-8 text-purple-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                         <path fill-rule="evenodd" d="M5.617 2.076a1 1 0 0 1 1.09.217L8 3.586l1.293-1.293a1 1 0 0 1 1.414 0L12 3.586l1.293-1.293a1 1 0 0 1 1.414 0L16 3.586l1.293-1.293A1 1 0 0 1 19 3v18a1 1 0 0 1-1.707.707L16 20.414l-1.293 1.293a1 1 0 0 1-1.414 0L12 20.414l-1.293 1.293a1 1 0 0 1-1.414 0L8 20.414l-1.293 1.293A1 1 0 0 1 5 21V3a1 1 0 0 1 .617-.924ZM9 7a1 1 0 0 0 0 2h6a1 1 0 1 0 0-2H9Zm0 4a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2H9Zm0 4a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2H9Z" clip-rule="evenodd"/>
